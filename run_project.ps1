4ac215c095247556af02b95e83769c4da03c6305
Lyagoubi Mohamed <mohamedlyagoubi@gmail.com>
2026-01-06 11:02:35 +0100

    fichier run

--- run_project.ps1
+++ run_project.ps1
@@ -1,6 +1,6 @@
 docker compose up -d
 
-python -m src.pipeline --source books --pages 1
-python -m src.pipeline --source quotes --pages 1
-python -m src.pipeline --source api --query "Lille" --limit 3
-python -m src.pipeline --source partners --partners-file data/partenaire_librairies.xlsx --geocode-partners
+python -m src.pipeline --source books --pages 1 # Scraping livres 1 page
+python -m src.pipeline --source quotes --pages 1 # Scraping citations 1 page
+python -m src.pipeline --source api --query "Lille" --limit 3 # API Adresse - 3 resultats
+python -m src.pipeline --source partners --partners-file data/partenaire_librairies.xlsx --geocode-partners 
